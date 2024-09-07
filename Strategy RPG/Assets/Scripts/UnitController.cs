847a0c15ab71970fc236803df19c8aedb16c2635
TheGingerGamer <143351023+TheGingerGamer@users.noreply.github.com>
2024-09-06 20:28:11 -0500

    Implemented grid tutorial

---
+++ Strategy RPG/Assets/Scripts/UnitController.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour
+{
+
+    Transform selectedUnit;
+    bool unitSelected = false;
+    GridManager gridManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        gridManager = FindFirstObjectByType<GridManager>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            bool hasHit = Physics.Raycast(ray, out hit);
+
+            if(hasHit)
+            {
+                if(hit.transform.tag == "Tile")
+                {
+                    if(unitSelected)
+                    {
+                        Vector2Int targetCords = hit.transform.GetComponent<Labeller>().cords;
+                        Vector2Int startCords = new Vector2Int((int) selectedUnit.position.x, (int) selectedUnit.position.y) / gridManager.UnityGridSize;
+
+                        selectedUnit.transform.position = new Vector3(targetCords.x, selectedUnit.position.y, targetCords.y);
+                    }
+                }
+
+                if(hit.transform.tag == "Player")
+                {
+                    selectedUnit = hit.transform;
+                    unitSelected = true;
+                }
+            }
+        }
+    }
+}
